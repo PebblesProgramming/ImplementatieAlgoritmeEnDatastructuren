ece1e67f09ae86a987aef527c5725d2c06bf08a6
Milan <milan0709@hotmail.nl>
2025-11-13 20:26:17 +0100

    Generics for Lijst1, a whole new wooorrlldd

--- Implementation/Program.cs
+++ Implementation/Program.cs
@@ -7,8 +7,23 @@ namespace Implementation
     {
         static void Main(string[] args)
         {
-           double result = Test.Add(1, 1);
+            double result = Test.Add(1, 1);
             Console.WriteLine($"1 + 1 = {result}");
+
+            var test = new Lijst1<int>();
+            test.add(1);
+            test.add(2);
+            test.add(10);
+
+           
+        }
+
+        public void test(Lijst1<int> list)
+        {
+            for (int i = 0; i < list.size(); i++)
+            {
+                Console.WriteLine(list.get(i));
+            }
         }
 
     }
